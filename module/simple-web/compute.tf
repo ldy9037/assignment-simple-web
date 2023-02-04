eece1f2acda93b85a9a58d45f60d3ae448ed332f
emadam <ldy9037@naver.com>
2023-02-04 12:07:13 +0900

    refactor: ALB 관련 resource를 별도의 파일로 분리

--- module/simple-web/compute.tf
+++ module/simple-web/compute.tf
@@ -1,23 +1,3 @@
-resource "aws_lb" "main" {
-  name               = var.alb_name
-  internal           = false
-  load_balancer_type = "application"
-  #security_groups    = []
-  subnets = [for subnet in aws_subnet.public : subnet.id]
-
-  enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.logs.bucket
-    prefix  = "alb/${var.alb_name}"
-    enabled = var.alb_log_enble
-  }
-
-  depends_on = [
-    aws_s3_bucket_policy.alb_logging
-  ]
-}
-
 data "aws_ami" "ubuntu_22" {
   most_recent = true
 
